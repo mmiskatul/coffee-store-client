ab02cd18111d64bf77cf0e2bd9cb3dea61ada0fe
Md Miskatul Masabi <masabimisakat@gamil.com>
2025-09-16 13:23:52 +0600

    update my footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,31 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import bgimage from '../../assets/Rectangle 18.png'
 
 function Footer() {
   return (
     <div>
-      <footer className="footer sm:footer-horizontal bg-white text-black p-10 ">
-        <nav>
-          <h6 className="footer-title">Services</h6>
-          <Link className="link link-hover">Branding</Link>
-          <Link className="link link-hover">Design</Link>
-          <Link className="link link-hover">Marketing</Link>
-          <Link className="link link-hover">Advertisement</Link>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Company</h6>
-          <Link className="link link-hover">About us</Link>
-          <Link className="link link-hover">Contact</Link>
-          <Link className="link link-hover">Jobs</Link>
-          <Link className="link link-hover">Press kit</Link>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Legal</h6>
-          <Link className="link link-hover">Terms of use</Link>
-          <Link className="link link-hover">Privacy policy</Link>
-          <Link className="link link-hover">Cookie policy</Link>
-        </nav>
-        <form>
+      <footer className="w-full footer sm:footer-horizontal bg-white text-black p-10 " style={{backgroundImage: `url(${bgimage})` }}>
+        
+       <div className="w-1/2" >
+
+       </div>
+       <div>
+         <form>
           <h6 className="footer-title">Newsletter</h6>
           <fieldset className="w-80">
             <label>Enter your email address</label>
@@ -39,6 +25,7 @@ function Footer() {
             </div>
           </fieldset>
         </form>
+       </div>
       </footer>
     </div>
   );
