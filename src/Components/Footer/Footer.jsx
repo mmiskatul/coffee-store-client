09be912e3ddc59d25ccb20a244e5f1dde85a4c1a
Md Miskatul Masabi <masabimisakat@gamil.com>
2025-09-16 13:10:43 +0600

    add footer section

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
