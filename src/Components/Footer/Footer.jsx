1442ff70dfa8e904f55b7b2e0cc30671fc2f2bea
Md Miskatul Masabi <masabimisakat@gamil.com>
2025-09-16 13:11:28 +0600

    add footer title

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,7 +11,14 @@ function Footer() {
           <a className="link link-hover">Marketing</a>
           <a className="link link-hover">Advertisement</a>
         </nav>
-        
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+      
       </footer>
     </div>
   );
