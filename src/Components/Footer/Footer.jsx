8f06010f0c300af37d6728805d165b23b254fac9
Md Miskatul Masabi <masabimisakat@gamil.com>
2025-09-16 13:13:06 +0600

    add feedback form

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -24,7 +24,20 @@ function Footer() {
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
         </nav>
-       
+        <form>
+          <h6 className="footer-title">Newsletter</h6>
+          <fieldset className="w-80">
+            <label>Enter your email address</label>
+            <div className="join">
+              <input
+                type="text"
+                placeholder="username@site.com"
+                className="input input-bordered join-item"
+              />
+              <button className="btn btn-primary join-item">Subscribe</button>
+            </div>
+          </fieldset>
+        </form>
       </footer>
     </div>
   );
