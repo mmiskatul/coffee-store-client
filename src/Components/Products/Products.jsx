bc1967bc4111eb64a1815463931a768ed2a0bb4b
Md Miskatul Masabi <masabimisakat@gamil.com>
2025-09-16 11:15:33 +0600

    update the products

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Products = () => {
   const [products, setProducts] = useState(null);
   useEffect(() => {
-    fetch("http://localhost:5000/products")
+    fetch("http://localhost:5001/products")
       .then((res) => res.json())
       .then((data) => setProducts(data))
       .catch((err) => console.error("Fetch error:", err));
@@ -16,28 +16,35 @@ const Products = () => {
       <Link to="/addcoffee" className="btn  bg-[#E3B577] text-[#] text-normal">
         Add Coffee
       </Link>
-      <div>
-        {products.map((product, idx) => (
-          <div className="grid grid-cols-2 gap-4 p-4 border mb-4" key={idx}>
-            {/* picture */}
-            <div>
-              <img
-                src={product.imageUrl}
-                alt={product.name}
-                className="w-full h-auto"
-              />
-            </div>
+      <div className="Raleway grid grid-cols-2 gap-4 p-4 border mb-4">
+        {products && products.length > 0 ? (
+          products.map((product, idx) => (
+            <div
+              className="Raleway flex  gap-4 p-4  mb-4"
+              key={idx}
+            >
+              {/* picture */}
+              <div className="size-20">
+                <img
+                  src={product.picUrl}
+                  alt={product.name}
+                  className="w-full h-auto"
+                />
+              </div>
 
-            {/* details */}
-            <div>
-              <h2 className="text-xl font-bold">{product.name}</h2>
-              <p>Chef: {product.chef}</p>
-              <p>Category: {product.category}</p>
-              <p>Taste: {product.taste}</p>
-              <p>{product.details}</p>
+              {/* details */}
+              <div>
+                <h2 className="text-xl font-bold">{product.name}</h2>
+                <p>Chef: {product.chef}</p>
+                <p>Category: {product.category}</p>
+                <p>Taste: {product.taste}</p>
+                <p>{product.details}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p>No products available</p>
+        )}
       </div>
     </div>
   );
